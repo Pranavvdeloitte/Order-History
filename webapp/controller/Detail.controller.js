c514c819a406cd0135043fd853c3037ae0ff5fd4
Pranav Garg <pranagarg@deloitte.com>
2026-02-06 10:05:01 +0000

    changes done

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -2,9 +2,8 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "sap/ndc/BarcodeScanner",
     "sap/m/MessageBox"
-], (Controller, Filter, FilterOperator, BarcodeScanner, MessageBox) => {
+], (Controller, Filter, FilterOperator, MessageBox) => {
     "use strict";
     var localDataModel;
     return Controller.extend("zorderhistory.controller.Detail", {
@@ -14,34 +13,17 @@ sap.ui.define([
         },
 
         _onObjectMatched: function (oEvent) { },
-        onScanBarcode: function () {
-            var that = this;
-            BarcodeScanner.scan(
-                function (oResult) {
-                    if (!oResult.cancelled) {
-                        this.getView().byId("idInputBarcode").setValue(oResult.text);
-                        MessageBox.show("We got a QR code\n" + "Result:" + oResult.text + "\n" + "Format:" + oResult.format + "\n");
-                    }
-                },
-                function (oError) {
-                    sap.m.MessageToast.show("Scan failed");
-                }
-            );
-        },
         formatQrCode: function (sUrl) {
             return "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="
                 + encodeURIComponent(sUrl);
         },
         onAfterRendering: function () {
-            // const Id = "MAT12345";
-            // const sDeepLink =
-            //     window.location.origin +
-            //     window.location.pathname +
-            //     "#/display?id=" +
-            //     Id;
-
-                const sDeepLink = "https://pranavvdeloitte.github.io/qr-demo/";
-
+            const Id = "MAT12345";
+            const sDeepLink = "https://pranavvdeloitte.github.io/Order-History/Qr.html";
+                // window.location.origin +
+                // window.location.pathname +
+                // "#/display?id=" +
+                // Id;
             this.getView().getModel("localDataModel").setProperty("/qrUrl", sDeepLink);
         }
     });
